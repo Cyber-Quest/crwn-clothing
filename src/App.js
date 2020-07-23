7d7eaaa8f0f3db26e774f24b9187b43f563597d7
Cyber-Quest <wesleu9983@gmail.com>
2020-07-23 14:43:53 -0300

    the menu-items component was updated and navigation routes were implemented

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Home from "./pages/home/home";
+
+const Hats = () => (
+  <div>
+    <h1>Hats page</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <Home></Home>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/hats" component={Hats} />
+      <Route exact path="/topics/:topicId" component={Hats} />
     </div>
   );
 }
